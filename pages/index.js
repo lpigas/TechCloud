e480d542f97ceb6d24bdb6ed69e4d8ec0ca1096d
lpigas <igornixdn@gmail.com>
2022-08-12 11:59:08 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Aboute() {
     <div id='Rectangle1' style={{background: 'linear-gradient(90deg, #F6F8FC 0%, #ECF0FA 100%)'}} className="absolute top-[8px] left-0 w-[1920px] h-[1080px] from-[#90deg, #F6F8FC_0%, #ECF0FA_100%]">
     <Header />
     </div>
-
+1
     <Footer />
     </div>
 
