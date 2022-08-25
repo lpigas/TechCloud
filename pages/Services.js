cad003b38b3c4d461db6323d8d89ee78cc6ebf1f
lpigas <igornixdn@gmail.com>
2022-08-25 21:25:56 +0300

    add login, add registration new user block

--- pages/Services.js
+++ pages/Services.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ServicesPage from "../scenes/Services/ServicesPage";
-import clientPromise from "../lib/mongodb";
+
 
 export default function Services({ data }) {
   return (
@@ -11,7 +11,7 @@ export default function Services({ data }) {
 }
 export async function getServerSideProps() {
   const { connectToDatabase } = require("../lib/mongodb");
-  const client = await clientPromise;
+
   let { db } = await connectToDatabase();
   const productInfo = await db.collection("Services").find().sort({}).toArray();
 
