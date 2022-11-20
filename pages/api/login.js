f98829b9e4c0d664038ad4ebcfa1d5f0b7d2df09
lpigas <igornixdn@gmail.com>
2022-11-20 15:38:52 +0200

    test

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,7 +4,6 @@ const ObjectId = require("mongodb").ObjectId;
 
 export default async function (req, res) {
   const { userAuth } = JSON.parse(req.body);
-  console.log(userAuth)
   const { db } = await connectToDatabase();
   const candidate = await db
     .collection("users")
